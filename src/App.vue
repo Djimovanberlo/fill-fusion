1e4dee17120120a7ec555c686020961642bac27b
Djimo van Berlo <djimovanberlo@gmail.com>
2026-06-26 12:16:15 +0200

    feat: add BeatCounter component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <div id="orientation-overlay">
     <p>Please rotate your device to landscape mode</p>
   </div>
+  <BeatCounter :beat="0" />
   <main>
     <div id="staff"></div>
   </main>
 </template>
 
 <script setup>
+import BeatCounter from './components/BeatCounter.vue'
 </script>
---
+++ src/components/BeatCounter.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="beat-counter">{{ beat + 1 }}</div>
+</template>
+
+<script setup>
+defineProps({
+  beat: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
